f2e7fdb45e99c8d1dcbf226412785b8c2112d626
Heather Stratton <hastratton@gmail.com>
2021-01-04 13:58:26 -0500

    js stuff

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+//https://www.sitepoint.com/community/t/looping-through-li-elements-in-ul/6049/2
+var ol = document.getElementById("work");
+var items = ul.getElementsByTagName("li");
+for (var i = 0; i <items.length; i++){
+
+}
+
+
+
 const links = [
     {
       label: "Week1 work & notes",
