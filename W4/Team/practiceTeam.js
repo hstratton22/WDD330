ab557601f1dc98db82984f79a4bb6a6cc4b563fe
Heather Stratton <hastratton@gmail.com>
2021-01-25 09:12:20 -0500

    tic

---
+++ W4/Team/practiceTeam.js
@@ -0,0 +1,10 @@
+function handleSquareTouch (){
+const player = "X";
+//document.
+
+}
+
+
+
+
+document.querySelectorAll('.square').forEach(square => square.addEventListener('touchend', handleSquareTouch));
