f68e5a28f9cde6371ece39a6142aad2e8c976836
Heather Stratton <hastratton@gmail.com>
2021-02-02 16:06:17 -0500

    MVC hike to click

--- W5/MVC/HikesController.js
+++ W5/MVC/HikesController.js
@@ -2,7 +2,8 @@
 import HikeModel from './hikeModel.js';
 import HikesView from './hikesView.js';
 
-// Just like with the view we should organize the functions we need to our controller. Let's use a class for this one
+// Just like with the view we should organize the functions we need to our controller. 
+//Let's use a class for this one
 
 export default class HikesController {
   // a class needs a constructor
@@ -28,12 +29,15 @@ export default class HikesController {
       this.showHikeList();
     };
   }
-  // in order to show the details of a hike ontouchend we will need to attach a listener AFTER the list of hikes has been built. The function below does that.
+  // in order to show the details of a hike ontouchend we will need to attach a listener 
+  //AFTER the list of hikes has been built. The function below does that.
   addHikeListener() {
-    // We need to loop through the children of our list and attach a listener to each, remember though that children is a nodeList...not an array. So in order to use something like a forEach we need to convert it to an array.
+    // We need to loop through the children of our list and attach a listener to each, 
+    //remember though that children is a nodeList...not an array. So in order to use something 
+    //like a forEach we need to convert it to an array.
     const childrenArray = Array.from(this.parentElement.children);
     childrenArray.forEach(child => {
-      child.addEventListener('touchend', e => {
+      child.addEventListener('click', e => {//touchend
         // why currentTarget instead of target?
         this.showOneHike(e.currentTarget.dataset.name);
       });
