9522615f45ca707bdd167af3bd7c390b338b9359
Heather Stratton <hastratton@gmail.com>
2021-03-01 14:55:27 -0500

    W10Team files and project folder

---
+++ W10/earthquake/index.js
@@ -0,0 +1,14 @@
+import QuakesController from './QuakesController.js';
+import { getJSON, getLocation } from './utilities.js';
+
+
+const baseUrl ='https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
+
+function getQuakes(baseUrl) {
+    return getJSON(baseUrl);
+  }
+  getQuakes(baseUrl);
+
+
+
+
