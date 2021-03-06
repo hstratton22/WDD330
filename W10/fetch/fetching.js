a970137c88a1c9e463d67e173049394693a2f3ea
Heather Stratton <hastratton@gmail.com>
2021-03-06 15:37:21 -0500

    SPAexample

--- W10/fetch/fetching.js
+++ W10/fetch/fetching.js
@@ -3,9 +3,9 @@
 fetch('http://example.com/movies.json')
   .then(response => response.json())
   .then(data => console.log(data));
-  
 
-  // Example POST method implementation:
+
+// Example POST method implementation:
 async function postData(url = '', data = {}) {
   // Default options are marked with *
   const response = await fetch(url, {
@@ -45,10 +45,11 @@ fetch('https://example.com', {
 //To instead ensure browsers don’t include credentials in the request, use credentials: 'omit'.
 
 fetch('https://example.com', {
-    credentials: 'omit'
-  })
-  //Use fetch() to POST JSON-encoded data.
-  const data = { username: 'example' };
+  credentials: 'omit'
+})
+
+//Use fetch() to POST JSON-encoded data.
+const data = { username: 'example' };
 
 fetch('https://example.com/profile', {
   method: 'POST', // or 'PUT'
@@ -57,13 +58,13 @@ fetch('https://example.com/profile', {
   },
   body: JSON.stringify(data),
 })
-.then(response => response.json())
-.then(data => {
-  console.log('Success:', data);
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 
 //Files can be uploaded using an HTML <input type="file" /> input element, FormData() and fetch().
 
@@ -77,13 +78,14 @@ fetch('https://example.com/profile/avatar', {
   method: 'PUT',
   body: formData
 })
-.then(response => response.json())
-.then(result => {
-  console.log('Success:', result);
-})
-.catch(error => {
-  console.error('Error:', error);
-});
+  .then(response => response.json())
+  .then(result => {
+    console.log('Success:', result);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
 //multiple files: Files can be uploaded using an HTML <input type="file" multiple /> input element, FormData() and fetch().
 
 const formData = new FormData();
@@ -98,13 +100,13 @@ fetch('https://example.com/posts', {
   method: 'POST',
   body: formData,
 })
-.then(response => response.json())
-.then(result => {
-  console.log('Success:', result);
-})
-.catch(error => {
-  console.error('Error:', error);
-});
+  .then(response => response.json())
+  .then(result => {
+    console.log('Success:', result);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
 //text file line-by-line
 //The chunks that are read from a response are not broken neatly at line boundaries and are Uint8Arrays, 
 //not strings. If you want to fetch a text file and process it line by line, it is up to you to handle 
@@ -112,64 +114,64 @@ fetch('https://example.com/posts', {
 //it assumes the text is UTF-8, and doesn't handle fetch errors).
 
 async function* makeTextFileLineIterator(fileURL) {
-    const utf8Decoder = new TextDecoder('utf-8');
-    const response = await fetch(fileURL);
-    const reader = response.body.getReader();
-    let { value: chunk, done: readerDone } = await reader.read();
-    chunk = chunk ? utf8Decoder.decode(chunk) : '';
-  
-    const re = /\n|\r|\r\n/gm;
-    let startIndex = 0;
-    let result;
-  
-    for (;;) {
-      let result = re.exec(chunk);
-      if (!result) {
-        if (readerDone) {
-          break;
-        }
-        let remainder = chunk.substr(startIndex);
-        ({ value: chunk, done: readerDone } = await reader.read());
-        chunk = remainder + (chunk ? utf8Decoder.decode(chunk) : '');
-        startIndex = re.lastIndex = 0;
-        continue;
+  const utf8Decoder = new TextDecoder('utf-8');
+  const response = await fetch(fileURL);
+  const reader = response.body.getReader();
+  let { value: chunk, done: readerDone } = await reader.read();
+  chunk = chunk ? utf8Decoder.decode(chunk) : '';
+
+  const re = /\n|\r|\r\n/gm;
+  let startIndex = 0;
+  let result;
+
+  for (; ;) {
+    let result = re.exec(chunk);
+    if (!result) {
+      if (readerDone) {
+        break;
       }
-      yield chunk.substring(startIndex, result.index);
-      startIndex = re.lastIndex;
+      let remainder = chunk.substr(startIndex);
+      ({ value: chunk, done: readerDone } = await reader.read());
+      chunk = remainder + (chunk ? utf8Decoder.decode(chunk) : '');
+      startIndex = re.lastIndex = 0;
+      continue;
     }
-    if (startIndex < chunk.length) {
-      // last line didn't end in a newline char
-      yield chunk.substr(startIndex);
-    }
+    yield chunk.substring(startIndex, result.index);
+    startIndex = re.lastIndex;
   }
-  
-  async function run() {
-    for await (let line of makeTextFileLineIterator(urlOfFile)) {
-      processLine(line);
-    }
+  if (startIndex < chunk.length) {
+    // last line didn't end in a newline char
+    yield chunk.substr(startIndex);
   }
-  
-  run();
-  //check for successful:
-  //A fetch() promise will reject with a TypeError when a network error is encountered or CORS 
-  //is misconfigured on the server-side, although this usually means permission issues or similar 
-  //— a 404 does not constitute a network error, for example. An accurate check for a successful fetch() 
-  //would include checking that the promise resolved, then checking that the Response.ok property has a 
-  //value of true. The code would look something like this:
+}
+
+async function run() {
+  for await (let line of makeTextFileLineIterator(urlOfFile)) {
+    processLine(line);
+  }
+}
+
+run();
+//check for successful:
+//A fetch() promise will reject with a TypeError when a network error is encountered or CORS 
+//is misconfigured on the server-side, although this usually means permission issues or similar 
+//— a 404 does not constitute a network error, for example. An accurate check for a successful fetch() 
+//would include checking that the promise resolved, then checking that the Response.ok property has a 
+//value of true. The code would look something like this:
 
 fetch('flowers.jpg')
-.then(response => {
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  return response.blob();
-})
-.then(myBlob => {
-  myImage.src = URL.createObjectURL(myBlob);
-})
-.catch(error => {
-  console.error('There has been a problem with your fetch operation:', error);
-});
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.blob();
+  })
+  .then(myBlob => {
+    myImage.src = URL.createObjectURL(myBlob);
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
 //supplying own request
 //Instead of passing a path to the resource you want to request into the fetch() call, 
 //you can create a request object using the Request() constructor, and pass that in as a 
@@ -189,8 +191,8 @@ fetch(myRequest)
   .then(myBlob => {
     myImage.src = URL.createObjectURL(myBlob);
   });
-  //Request() accepts exactly the same parameters as the fetch() method. 
-  //You can even pass in an existing request object to create a copy of it:
+//Request() accepts exactly the same parameters as the fetch() method. 
+//You can even pass in an existing request object to create a copy of it:
 
 const anotherRequest = new Request(myRequest, myInit);
 //This is pretty useful, as request and response bodies are one use only. Making a copy 
@@ -206,12 +208,12 @@ myHeaders.append('Content-Length', content.length.toString());
 myHeaders.append('X-Custom-Header', 'ProcessThisImmediately');
 //OR
 const myHeaders = new Headers({
-    'Content-Type': 'text/plain',
-    'Content-Length': content.length.toString(),
-    'X-Custom-Header': 'ProcessThisImmediately'
-  });
-  //can be queried
-  console.log(myHeaders.has('Content-Type')); // true
+  'Content-Type': 'text/plain',
+  'Content-Length': content.length.toString(),
+  'X-Custom-Header': 'ProcessThisImmediately'
+});
+//can be queried
+console.log(myHeaders.has('Content-Type')); // true
 console.log(myHeaders.has('Set-Cookie')); // false
 myHeaders.set('Content-Type', 'text/html');
 myHeaders.append('X-Custom-Header', 'AnotherValue');
@@ -233,20 +235,21 @@ try {
 //type is correct before you process it further.
 fetch(myRequest)
   .then(response => {
-     const contentType = response.headers.get('content-type');
-     if (!contentType || !contentType.includes('application/json')) {
-       throw new TypeError("Oops, we haven't got JSON!");
-     }
-     return response.json();
+    const contentType = response.headers.get('content-type');
+    if (!contentType || !contentType.includes('application/json')) {
+      throw new TypeError("Oops, we haven't got JSON!");
+    }
+    return response.json();
   })
   .then(data => {
-      /* process your data further */
+    /* process your data further */
   })
   .catch(error => console.error(error));
-  //ServiceWorkers respondWith()
-  const myBody = new Blob();
 
-addEventListener('fetch', function(event) {
+//ServiceWorkers respondWith()
+const myBody = new Blob();
+
+addEventListener('fetch', function (event) {
   // ServiceWorker intercepting a fetch
   event.respondWith(
     new Response(myBody, {
@@ -254,6 +257,7 @@ addEventListener('fetch', function(event) {
     })
   );
 });
+
 //The Response() constructor takes two optional arguments — 
 //a body for the response, and an init object (similar to the one that Request() accepts.)
 
@@ -264,9 +268,10 @@ fetch('/login', {
   method: 'POST',
   body: form
 });
+
 //feature detection
 if (window.fetch) {
-    // run my fetch request here
-  } else {
-    // do something with XMLHttpRequest?
-  }
+  // run my fetch request here
+} else {
+  // do something with XMLHttpRequest?
+}
