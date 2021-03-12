0241b090c0d6d64eeae25bb65885b92910980de4
Heather Stratton <hastratton@gmail.com>
2021-03-12 15:35:20 -0500

    fix for templeQuiz

--- project/templeQuiz/templeQuiz.js
+++ project/templeQuiz/templeQuiz.js
@@ -1,9 +1,11 @@
+//problem with line7
 const url="templeQuiz.json";
 
 fetch(url)
 .then(result => result.json())
 .then(quiz => {
-    view.start.addEventListener('click', () => game.start(quiz.questions), false);
+    view.start.addEventListener('click', () => game.start(quiz.temples), false);
+    //view.start.addEventListener('click', () => game.start(quiz.temples), false);
     view.response.addEventListener('click', (event)=> game.check(event), false);
 
 });
@@ -39,7 +41,7 @@ const view = {
     }, 
     setup(){
         this.show(this.question);
-        this.show(this.respons);
+        this.show(this.response);
         this.show(this.result);
         this.hide(this.start);
         this.render(this.result, "");
@@ -50,7 +52,8 @@ const view = {
         this.hide(this.response);
         this.show(this.start);
         
-    }, buttons(array){
+    }, 
+    buttons(array){
         return array.map(value => `<button>${value}</button`).join('');
 
     }
@@ -58,17 +61,48 @@ const view = {
 
 const game= {
     start(quiz){
-        this.score = 0;
-        this.questions = [...quiz];
+        //this.score = 0;
+        console.log("in start");
+        this.temples = [...quiz];
         view.setup();
         this.ask();
     },
     ask(name){
-        if(this.questions.length >2){
-            shuffle(this.question);
-            this.question= this.quesiton.pop();
-           // const options = [this.questions[0].name, this.temples[1].name, this.temples.name];
+        if(this.temples.length >2){
+            shuffle(this.temples);
+            this.question= this.temples.pop();
+            const options = [this.temples[0].name, this.temples[1].name, this.question.name];
+            shuffle(options);
+
+            const questionImage = `<img src="${this.question.imageSrc}" alt="temple image">`;
+
+            view.render(view.question,questionImage);
+            view.render(view.response, view.buttons(options));
+
         }
+        else {
+            this.gameOver();
+        }
+    },
+    check(event){
+        const response=event.target.textContent;
+        const answer = this.question.name;
+        if(response === answer){
+            console.log('correct');
+            view.render(view.result, 'Correct!',{'class':'correct'});
+            this.score++;
+            view.render(view.score, this.score);
+        } else {
+            console.log('wrong');
+            view.render(view.result,`Wrong! The correct answer was ${answer}`, {'class':'wrong'});
+
+        }
+        this.ask();
+    },
+    gameOver(){
+        console.log('in gameOver');
+        view.teardown();
+
     }
     
 }
