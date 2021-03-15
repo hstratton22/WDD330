97ff63232f10a868e1fb385a01b770bed65a3443
Heather Stratton <hastratton@gmail.com>
2021-03-15 10:37:49 -0400

    background size of templeQuiz large

--- project/templeQuiz/templeQuiz2.js
+++ project/templeQuiz/templeQuiz2.js
@@ -31,8 +31,6 @@ const view = {
   info: document.querySelector('#info'),
   start: document.querySelector('#start'),
   response: document.querySelector('#response'),
-  //timer: document.querySelector('#timer strong'),
-  //hiScore: document.querySelector('#hiScore strong'),
   render(target,content,attributes) {
       for(const key in attributes) {
         target.setAttribute(key, attributes[key]);
@@ -53,7 +51,6 @@ const view = {
     this.render(this.score,game.score);
     this.render(this.result,'');
     this.render(this.info,'');
-    //this.render(this.hiScore, game.hiScore());
   },
   teardown(){
     this.hide(this.question);
@@ -68,21 +65,12 @@ const view = {
 
 const game = {
   start(quiz){
-    //console.log('start() invoked');
     this.score = 0;
     this.temples = [...quiz];
     view.setup();
-    //this.secondsRemaining = 40;
-    //this.timer = setInterval( this.countdown , 1000 );
     this.ask();
   },
-  /*countdown() {
-    game.secondsRemaining--;
-    view.render(view.timer,game.secondsRemaining);
-      if(game.secondsRemaining <= 0) {
-        game.gameOver();
-      }
-  },*/
+
   ask(name){
     console.log('ask() invoked');
     if(this.temples.length > 2) {
@@ -93,8 +81,6 @@ const game = {
       
       const questionImage = `<img src="${this.question.imageSrc}" alt="temple image">`;
       
-      //console.log(this.question.imageSrc);
-      
       view.render(view.question,questionImage);
       view.render(view.response,view.buttons(options));
     }
@@ -103,32 +89,20 @@ const game = {
     }
   },
   check(event){
-    //console.log('check(event) invoked');
     const response = event.target.textContent;
     const answer = this.question.name;
     if(response === answer){
-      //console.log('correct');
       view.render(view.result,'Correct!',{'class':'correct'});
       this.score++;
       view.render(view.score,this.score);
     } else {
-      //console.log('wrong');
       view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
     }
     this.ask();
   },
   gameOver(){
-    //console.log('gameOver() invoked');
     view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
     view.teardown();
-   //clearInterval(this.timer);
-  } /*,
-hiScore(){
-    const hi = localStorage.getItem('highScore') || 0;
-    if(this.score > hi || hi === 0) {
-      localStorage.setItem('highScore',this.score);
-      view.render(view.info,'** NEW HIGH SCORE! **');
-    }
-    return localStorage.getItem('highScore');
-  }*/
+   
+  } 
 }
