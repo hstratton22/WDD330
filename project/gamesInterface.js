e5ecff5f924ac2df9675c3ab6445d63b3a64c99b
Heather Stratton <hastratton@gmail.com>
2021-03-15 10:13:06 -0400

    timer options to gameInterface

--- project/gamesInterface.js
+++ project/gamesInterface.js
@@ -1,37 +1,56 @@
 let menu=document.getElementById('menu');
 let quizBtn = document.createElement('button');
+let quizDiv = document.createElement('div');
+let quizTimer = document.createElement('button');
+let quizNoTimer = document.createElement('button');
 let matchBtn = document.createElement('button');
 let matchDiv = document.createElement('div');
 let matchVisible = document.createElement('button');
 let matchCovered = document.createElement('button');
 
 matchVisible.classList.add('buttonCtr');
-//matchVisible.classList.add('hidden')
+quizTimer.classList.add('buttonCtr');
+quizNoTimer.classList.add('buttonCtr');
 matchCovered.classList.add('buttonCtr');
-//matchCovered.classList.add('hidden');
+
 quizBtn.classList.add('button');
 matchBtn.classList.add('button');
 
 
 
 //quizBtn.innerText="Quiz";
-quizBtn.innerHTML=`<a href="QuizT/quizT.html">Quiz</a>`;
+quizBtn.innerText="Quiz";
 matchBtn.innerText="Matching";
-//matchCovered.innerText="Covered";
+
+quizTimer.innerHTML=`<a href="QuizT/quizT.html">Timer</a>`;
+quizNoTimer.innerHTML=`<a href="templeQuiz/templeQuiz.html">No Timer</a>`;
+
 matchVisible.innerHTML=`<a href="matchGame/index.html">Visible</a>`;
-//matchVisible.innerText="Visible";
 matchCovered.innerHTML=`<a href="matchGameBacks/index.html">Covered</a>`;
 
 matchDiv.classList.add('hidden');
+quizDiv.classList.add('hidden');
+
+quizDiv.appendChild(quizTimer);
+quizDiv.appendChild(quizNoTimer);
 matchDiv.appendChild(matchVisible);
 matchDiv.appendChild(matchCovered);
 
+
+
 menu.appendChild(quizBtn);
+menu.appendChild(quizDiv);
 menu.appendChild(matchBtn);
 menu.appendChild(matchDiv);
+
+function chooseQuiz(){
+    quizDiv.classList.remove('hidden');
+    quizDiv.classList.add('matchDiv');
+}
 
 function chooseMatching(){
     matchDiv.classList.remove('hidden');
     matchDiv.classList.add("matchDiv");
 }
+quizBtn.addEventListener('click', chooseQuiz);
 matchBtn.addEventListener('click', chooseMatching);
